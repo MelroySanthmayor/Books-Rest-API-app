8559dee4ce8ae6ec4e21f7a17e5b368533437226
MelroySanthmayor <melsanthmayor87@gmail.com>
2019-06-08 00:21:43 +0530

    Done with creation of the last API using simple javascript approach, tried the mongodb way, with lot of R & D, code commented in commit

--- controllers/BookController.js
+++ controllers/BookController.js
@@ -4,9 +4,8 @@ var CurrentUser = require('./UserController');
 var MongoClient = require('mongodb').MongoClient; // for writing queries to the db
 var url = 'mongodb://localhost/books';
 const JSON = require('circular-json');
-var ObjectId = require('mongodb').ObjectID;
+var ObjectId = require('mongodb').ObjectID;  // to query using user id while using mongoclient, works
 
-var User = CurrentUser.CurrentUser // get current user for adding books
 // books: {title: req.body.title, author: req.body.author}
 //Simple version, without validation or sanitation
 exports.test = function (req, res) {
@@ -53,36 +52,54 @@ exports.books_details = function (req, res, next) {
 };
 
 exports.book_filter = function (req, res, next) {
-    MongoClient.connect(url, { useNewUrlParser: true }, function(err, client) {
-        var db = client.db('books');
-        // if(db.collection('users').getIndexes().length > 0){
-        //     db.collection('users'.dropIndex())
-        // }
-        //db.collection('users').dropIndex("title_text_author_text");
-        //db.collection('users').createIndex( { title : "text" });
-        // query : $where: function() { return this._id === ObjectId(req.userId) },
-        //$text: {$search: req.query.text}}
-        db.collection('users').find({ $text: {$search: req.query.text}}).toArray(function(err, result) {  
-            if (err) throw err;  
-            console.log(req.userId, req.query.text);
-            res.send(result);
-        //noinspection JSDeprecatedSymbols
-        // cursor.each(function(err, item) {
+    Users.findById(req.userId, function (err, user) {
+        if (err) {
+            return next(err);
+        }
+        const User = user;
+        const FilteredUser = User.books.filter(data => data.title.includes(req.query.text));
+        var data = {
+            user_books : FilteredUser
+        }
+        res.status(200).send(data);
+    });
+
+};
+
+
+    // Below code was trial for text search with $text and indexing, lot of R & D on that, moving on to //a simpler approach, below appraoch works for the where condition
+
+
+
+    // MongoClient.connect(url, { useNewUrlParser: true }, function(err, client) {
+    //     var db = client.db('books');
+    //     // if(db.collection('users').getIndexes().length > 0){
+    //     //     db.collection('users'.dropIndex())
+    //     // }
+    //     //db.collection('users').dropIndex("title_text_author_text");
+    //     //db.collection('users').createIndex( { title : "text" });
+    //     // query : $where: function() { return this._id === ObjectId(req.userId) },
+    //     //$text: {$search: req.query.text}}
+    //     db.collection('users').find({$where: function() { return this._id === ObjectId(req.userId) }}).toArray(function(err, result) {  
+    //         if (err) throw err;  
+    //         console.log(req.userId, req.query.text);
+    //         res.send(result);
+    //     //noinspection JSDeprecatedSymbols
+    //     // cursor.each(function(err, item) {
         
-        //     if (item != null) {
+    //     //     if (item != null) {
                 
-        //     }
-        // });
+    //     //     }
+    //     // });
         
-            client.close();
-        });
+    //         client.close();
+    //     });
         // var json = JSON.stringify(cursor)
         // res.send(json);
         // client.close();
-   });
-} 
+   //});
 
-// result returns a list of books as follows
+// result returns a list of books as follows for the function books_details
 // {
 //     "user_books": [
 //         {
@@ -112,18 +129,3 @@ exports.book_filter = function (req, res, next) {
 //         }
 //     ]
 // }
-
-// .toArray(function(err, result) {  
-//     if (err) throw err;  
-//     console.log(req.query.text);
-//     res.send(result);
-// //noinspection JSDeprecatedSymbols
-// // cursor.each(function(err, item) {
-
-// //     if (item != null) {
-        
-// //     }
-// // });
-
-//     client.close();
-// });
